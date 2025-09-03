f81a2988b030b445859e5e3d9c66c8bb20d08fe8
heitorl <juniorio01@hotmail.com>
2025-09-03 20:45:15 -0300

    add procfile

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from app.routes import init_app as init_routes
-
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     init_routes(app)
 
